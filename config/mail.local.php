f291390931735c00ab1d43473ead84a89cf2fb7e
Othmaan100 <usmankamis1@gmail.com>
2026-09-09 17:27:46 +0100

    Email Config Update

---
+++ config/mail.local.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'       => 'smtp.gmail.com',   // or mail.tepan.org.ng
+    'port'       => 587,                // 587 = STARTTLS, 465 = SMTPS
+    'encryption' => 'tls',              // 'tls' or 'ssl'
+    'username'   => 'youraccount@gmail.com',
+    'password'   => 'your-app-password',
+    'from_email' => 'no-reply@tepan.org.ng',
+    'from_name'  => 'TEPAN',
+];
